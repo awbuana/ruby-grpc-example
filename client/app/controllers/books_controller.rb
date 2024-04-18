8e62915dc046da15495f2aabacd0b83c417b43c6
Agni Wira Buana <agni.wira@bukalapak.com>
2024-04-18 19:39:32 +0700

    feat: test

--- client/app/controllers/books_controller.rb
+++ client/app/controllers/books_controller.rb
@@ -20,7 +20,76 @@ class BooksController < ApplicationController
   private
 
   def stub
-    Books::BookService::Stub.new('grpc-server:50051', :this_channel_is_insecure)
+    # RubyLogger defines a logger for gRPC based on the standard ruby logger.
+    module RubyLogger
+      def logger
+        LOGGER
+      end
+
+      LOGGER = Logger.new(STDOUT)
+      LOGGER.level = Logger::DEBUG
+    end
+
+    # GRPC is the general RPC module
+    module GRPC
+      # Inject the noop #logger if no module-level logger method has been injected.
+      extend RubyLogger
+    end
+
+
+    require 'books_services_pb'
+    stub_retry = Books::BookService::Stub.new('localhost:50051', :this_channel_is_insecure, timeout: 10, channel_args: {
+      "grpc.enable_retries" => 1,
+      "grpc.service_config" => JSON.generate(
+        methodConfig: [
+          {
+            name: [{service: "books.BookService"}],
+            retryPolicy: {
+              retryableStatusCodes: ["UNAVAILABLE", "INTERNAL", "UNKNOWN", "NOT_FOUND"],
+              maxAttempts: 3,
+              initialBackoff: "1s",
+              backoffMultiplier: 2.0,
+              maxBackoff: "0.3s"
+            }
+          }
+        ]
+      )
+    })
+    r = stub_retry.get_books(Books::EmptyParams.new).to_h
+    r = stub_retry.get_book(Books::BookID.new(id: 69))
+
+    begin
+      r = stub_retry.get_book(Books::BookID.new(id: 69))
+    rescue GRPC::NotFound  => e
+      puts "operation cancel called - #{e} - #{e.to_status}"
+    end
+
+    stub = Books::BookService::Stub.new('localhost:50051', :this_channel_is_insecure, channel_args: {
+      "grpc.enable_retries" => 1,
+      "grpc.service_config" => JSON.generate(
+        methodConfig: [
+          {
+            name: [
+              {
+                service: "books.BookService",
+                method: "GetBooks"
+              }
+            ],
+            retryPolicy: {
+              retryableStatusCodes: ["UNAVAILABLE"],
+              maxAttempts: 3,
+              initialBackoff: "0.1s",
+              backoffMultiplier: 2.0,
+              maxBackoff: "0.3s"
+            }
+          }
+        ]
+      )
+    })
+    stub.get_books(Books::EmptyParams.new).to_h
+
+    stub2 = Books::BookService::Stub.new('localhost:50051', :this_channel_is_insecure)
+    stub2.get_books(Books::EmptyParams.new).to_h
   rescue GRPC::BadStatus => e
     abort "ERROR: #{e.message}"
   end
--- server/app/service/book_service.rb
+++ server/app/service/book_service.rb
@@ -5,11 +5,15 @@ require 'books_services_pb'
 # Purpose: Service to handle books actions
 class BookService < Books::BookService::Service
   def get_books(_request, _call)
+    puts "incoming request for get_books"
     books = datas
     Books::Books.new(books:)
   end
 
   def get_book(request, _call)
+    puts "incoming request for get_book"
+    raise GRPC::Unavailable if request.id.to_i == 69
+
     books = datas
     book = books.find { |b| b['id'] == request.id }
     raise GRPC::NotFound, 'Book not found' if book.nil?
